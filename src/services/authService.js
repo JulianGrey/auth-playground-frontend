9776e1d060fcf9b8ba99dbda7ba9506b056fa442
Julian Grey <julianrgrey@gmail.com>
2025-06-08 21:56:36 +0100

    Create authService

---
+++ src/services/authService.js
@@ -0,0 +1,28 @@
+const API_URL = 'http://localhost:3001';
+
+export async function login(username, password) {
+  const res = await fetch(`${API_URL}/login`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    credentials: 'include',
+    body: JSON.stringify({ username, password })
+  });
+  return res.json();
+}
+
+export async function logout() {
+  const res = await fetch(`${API_URL}/logout`, {
+    method: 'POST',
+    credentials: 'include'
+  });
+  return res.json();
+}
+
+export async function register(username, password) {
+  const res = await fetch(`${API_URL}/register`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ username, password })
+  });
+  return res.json();
+}
