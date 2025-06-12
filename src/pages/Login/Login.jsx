d7fc0cb820fe8198925a4e816b27558eaadd49d1
Julian Grey <julianrgrey@gmail.com>
2025-06-12 08:01:16 +0100

    Update Login styles to use CSS module syntax

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -4,12 +4,12 @@ export default function Login() {
   return (
     <>
       <div className={styles.login}>
-        <div className={`form-input ${styles['login-input']}`}>
-          <label htmlFor="form-login">Login</label>
+        <div className={`${styles['form-input']} ${styles['login-input']}`}>
+          <label className={styles['form-login']} htmlFor='form-login'>Login</label>
           <input type="text" name="form-login" placeholder="User name" />
         </div>
-        <div className={`form-input ${styles['password-input']}`}>
-          <label htmlFor="form-password">Password</label>
+        <div className={`${styles['form-input']} ${styles['password-input']}`}>
+          <label className={styles['form-password']} htmlFor='form-password'>Password</label>
           <input type="text" name="form-password" placeholder="Password" />
         </div>
       </div>
