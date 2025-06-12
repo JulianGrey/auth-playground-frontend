65b7f6b88bb607c501efdee5af55589a049acb7a
Julian Grey <julianrgrey@gmail.com>
2025-06-12 14:15:17 +0100

    Modify UI for Login and Register flows

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,22 +1,39 @@
+import { useState } from 'react';
 import styles from './Login.module.scss';
 
 export default function Login() {
+  const [existing, setExisting] = useState(true);
+
+  function handleExisting(isExisting) {
+    setExisting(isExisting);
+  }
+
   return (
-    <>
-      <div className={styles.login}>
-        <div className={styles['registered-status']}>
-          <span>Existing user</span>
-          <span>New user</span>
-        </div>
-        <div className={`${styles['form-input']} ${styles['login-input']}`}>
-          <label className={styles['form-login']} htmlFor='form-login'>Login</label>
-          <input type="text" name="form-login" placeholder="User name" />
-        </div>
-        <div className={`${styles['form-input']} ${styles['password-input']}`}>
-          <label className={styles['form-password']} htmlFor='form-password'>Password</label>
-          <input type="text" name="form-password" placeholder="Password" />
-        </div>
+    <div className={styles.login}>
+      <div className={styles['registered-status']}>
+        <span
+          className={existing ? styles.active : ''}
+          onClick={() => handleExisting(true)}
+        >Existing user</span>
+        <span
+          className={existing ? '' : styles.active}
+          onClick={() => handleExisting(false)}
+        >New user</span>
       </div>
-    </>
+      <div className={`${styles['form-input']} ${styles['login-input']}`}>
+        <label className={styles['form-login']} htmlFor='form-login'>Login</label>
+        <input type="text" name="form-login" placeholder="User name" />
+      </div>
+      <div className={`${styles['form-input']} ${styles['password-input']}`}>
+        <label className={styles['form-password']} htmlFor='form-password'>Password</label>
+        <input type="text" name="form-password" placeholder="Password" />
+      </div>
+      { !existing && <div className={`${styles['form-input']} ${styles['password-confirm-input']}`}>
+          <label className={styles['form-password-confirm']} htmlFor='form-password-confirm'>Password</label>
+          <input type="text" name="form-password-confirm" placeholder="Confirm Password" />
+        </div>
+      }
+      { existing ? <button>Login</button> : <button>Register</button> }
+    </div>
   );
 }
